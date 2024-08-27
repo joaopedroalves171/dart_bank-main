61cd7711cf2700a846aa831bb65a31ad4f88bace
jp <joao.110122@gmail.com>
2024-08-27 14:31:36 -0300

    first commit

---
+++ test/dart_bank_test.dart
@@ -0,0 +1,56 @@
+import 'package:dart_bank/models/premium_account.dart';
+import 'package:dart_bank/models/simple_account.dart';
+import 'package:dart_bank/services/transfer_service.dart';
+import 'package:test/test.dart';
+import 'package:dart_bank/models/account.dart';
+
+void main() {
+  group('transferService', () {
+    late Account account1;
+    late Account account2;
+
+    setUp(() {
+      account1 = SimpleAccount('Diego', 1001, 1000.0);
+      account2 = PremiumAccount(
+        'Arthur',
+        1002,
+        500.0,
+        cashBack: 10.0,
+      );
+    });
+
+    test('should transfer money from one simple account to premium account', () {
+      transferService(account1, account2, 200.0);
+
+      expect(account1.getBalance(), 800.0);
+      expect(account2.getBalance(), 700.0);
+    });
+
+    test('should not allow transfer to the same account', () {
+      expect(() => transferService(account1, account1, 200.0), throwsException);
+    });
+
+    test('should not allow transfer of amount greater than balance', () {
+      expect(() => transferService(account1, account2, 2000.0), throwsException);
+    });
+
+    test('should not allow transfer of zero or negative amount', () {
+      expect(() => transferService(account1, account2, 0.0), throwsException);
+      expect(() => transferService(account1, account2, -100.0), throwsException);
+    });
+  });
+}
+
+void transferService(Account from, Account to, double amount) {
+  if (from == to) {
+    throw Exception('Cannot transfer to the same account');
+  }
+  if (amount <= 0) {
+    throw Exception('Transfer amount must be greater than zero');
+  }
+  if (from.getBalance() < amount) {
+    throw Exception('Insufficient balance');
+  }
+  from.withdraw(amount);
+  to.deposit(amount);
+}
